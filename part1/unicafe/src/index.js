65c7a563c941ee2319e38aa89cb0ff5dc9409a13
wrthlor <nickster179@gmail.com>
2020-12-06 00:32:40 -0600

    Completed ex1 unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,38 +8,71 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+//Statistics component
+const Statistic = ({text, counter}) => (
+  <tr>
+    <td>{text}: </td>
+    <td>{counter}</td>
+  </tr>
+)
+
+const Statistics = ({good, neutral, bad}) => {  
+  // Calculating total, average, percent positive
+  const total = good + neutral + bad;     //total count
+  const avg = (good - bad) / total;       //average VALUE
+  const pos = good / total * 100;         //percentage of positive counts
+
+  if (total === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  else {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <Statistic text='Good' counter={good} />
+            <Statistic text='Neutral' counter={neutral} />
+            <Statistic text='Bad' counter={bad} />
+            <Statistic text='All' counter={total} />
+            <Statistic text='Average' counter={avg.toFixed(3)} />
+            <Statistic text='Positive' counter={pos.toFixed(2) + "%"} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] =  useState(0);
 
   //Incrememts good counter
-  const handleGood = () => {
+  const goodCounter = () => {
     setGood(good + 1);
   }
 
   //Increments neutral counter
-  const handleNeutral = () => {
+  const neutralCounter = () => {
     setNeutral(neutral + 1);
   }
 
   //Increments bad counter
-  const handleBad = () => {
+  const badCounter = () => {
     setBad(bad + 1);
   }
-
 
   return (
     <div>
       <h1>Please provide feedback!</h1>
-      <Button onClick={handleGood} text='Good' />
-
-      <Button onClick={handleNeutral} text='Neutral' />
-
-      <Button onClick={handleBad} text='Bad' />
-
+      <Button onClick={goodCounter} text='Good' />
+      <Button onClick={neutralCounter} text='Neutral' />
+      <Button onClick={badCounter} text='Bad' />
       <h2>The stats:</h2>
-      <p>Good: {good} <br/> Neutral: {neutral} <br/> Bad: {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
