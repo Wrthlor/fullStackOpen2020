2bc4022b75e674117ff980162d8d9afca968a3d0
wrthlor <nickster179@gmail.com>
2020-12-08 23:19:00 -0600

    Exercise 2.4

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -46,32 +46,57 @@ const Course = ({ course }) => {
   )
 };
 
+// Exercise 2.4
+// Supports arbitrary number of courses -> use Array.prototype.map() method to create array of courses
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map( (indivCourse) => <Course key={indivCourse.id} course={indivCourse} /> )}
     </div>
   )
 };
