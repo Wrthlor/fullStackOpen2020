3e91f9c4271140b4f86d8c91c43e06e5b624f4f3
wrthlor <nickster179@gmail.com>
2020-12-09 18:10:01 -0600

    First commit, ex2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+/*
+const Note = ({ note }) => {
+    return (
+      <li>{note.content}</li>
+    )
+  }
+  */
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [ newName, setNewName ] = useState('');
+
+  // Function to deal with people, add new person to "phonebook"
+  const addPerson = (event) => {
+    event.preventDefault();
+    // Person object (contains name, number, id, etc..
+    const personObject = {
+        name: newName,
+    }
+
+    /*
+    console.log(persons);
+    console.log(persons[0]);
+    console.log(persons[0].name);
+    console.log("typeOf: ", typeof(newName), " Value: ", newName);
+    */
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  // Function to deal with new names
+  const handleNewName = (event) => {
+    //console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>
+          {persons.map((name) => {
+              return <ul>{name.name}</ul>;
+          })}
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
