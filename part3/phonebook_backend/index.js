39c5d6029c5f32a7948676c2b13bd9999f6f997a
wrthlor <nickster179@gmail.com>
2021-01-06 17:17:57 -0600

    Exercise 3.3 - Phonebook backend step 3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -32,6 +32,18 @@ app.get('/api/persons', (req, res) => {
     res.json(phonebook);
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = phonebook.find(p => p.id === id);
+
+    if(person) {
+        res.json(person);
+    }
+    else {
+        res.status(404).end();
+    }
+})
+
 app.get('/info', (req, res) => {
     let message = (
         `<p>Phonebook has info for ${phonebook.length} people</p>` + 
