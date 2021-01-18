3d39e691f5b59444a5392a2dcb768c81460b673f
wrthlor <nickster179@gmail.com>
2021-01-17 18:36:20 -0600

    Exercise 3.13 - Phonebook database - Step 1 - Fetching from MongoDB database

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const Person = require('./models/person');
 
 const cors = require('cors');
 const morgan = require('morgan');
@@ -7,34 +9,6 @@ const morgan = require('morgan');
 app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
-
-let phonebook = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"        
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"        
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    },
-    {
-        id: 5,
-        name: "Levi Ackerman",
-        number: "1337"
-    }
-]
 
 // Create custom token to output data sent in HTTP POST requests only
 morgan.token('person', (req, res) => {
@@ -55,35 +29,30 @@ app.get('/', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    let message = (
-        `<p>Phonebook has info for ${phonebook.length} people</p>` + 
-        `<p>${new Date().toString()}</p>`
-    );
-    res.send(message)
-
+    Person
+        .find({})
+        .then(people => {
+            res.send(
+                `<p>Phonebook has info for ${people.length} people</p>` + 
+                `<p>${new Date().toString()}</p>`
+            );
+        })
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(phonebook);
+    Person
+        .find({})
+        .then(people => {
+            res.json(people);
+        })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = phonebook.find(p => p.id === id);
-
-    if(person) {
-        res.json(person);
-    }
-    else {
-        res.status(404).end();
-    }
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    phonebook = phonebook.filter(person => person.id !== id);
-
-    res.status(204).end();
+    Person
+        .findById(req.params.id)
+        .then(people => {
+            res.json(people);
+        })
 })
 
 app.post('/api/persons', (req, res) => {    
@@ -121,7 +90,14 @@ app.post('/api/persons', (req, res) => {
     res.json(person);
 })
 
-const PORT = process.env.PORT || 3001;
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    phonebook = phonebook.filter(person => person.id !== id);
+
+    res.status(204).end();
+})
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on portal ${PORT}`);
 })
