6a561714b32c6c195af0bd4b7757261e0f2022e4
wrthlor <nickster179@gmail.com>
2021-01-18 09:32:25 -0600

    Exercise 3.15 - Phonebook Database - Step 3 - Deleting entries from MongoDB database

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -88,10 +88,12 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    phonebook = phonebook.filter(person => person.id !== id);
-
-    res.status(204).end();
+    Person 
+        .findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => next(error));
 })
 
 const PORT = process.env.PORT;
