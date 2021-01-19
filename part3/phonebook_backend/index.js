9758db46cbc15139942d23bf122b6a98d67b23df
wrthlor <nickster179@gmail.com>
2021-01-19 15:44:23 -0600

    Exercise 3.22 - Lint configuration - Airbnb JS-style guide

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,121 +1,120 @@
 require('dotenv').config();
 const express = require('express');
+
 const app = express();
-const Person = require('./models/person');
 
 const cors = require('cors');
 const morgan = require('morgan');
+const Person = require('./models/person');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 
 // Create custom token to output data sent in HTTP POST requests only
-morgan.token('person', (req, res) => {
-    if (req.method === "POST") {
-        return JSON.stringify(req.body);
-    }
-    return null;
-})
+morgan.token('person', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body);
+  }
+  return null;
+});
 
 app.use(
-    morgan(
-        ":method :url :status :res[content-length] - :response-time ms :person"
-    )
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :person',
+  ),
 );
 
 app.get('/', (req, res) => {
-    res.send('<h1><b>Hello Wooorld!</b><\h1>');
-})
+  res.send('<h1><b>Hello Wooorld!</b></h1>');
+});
 
 app.get('/info', (req, res) => {
-    Person
-        .find({})
-        .then(people => {
-            res.send(
-                `<p>Phonebook has info for ${people.length} people</p>` + 
-                `<p>${new Date().toString()}</p>`
-            );
-        })
-        .catch(error => next(error));
-})
+  Person
+    .find({})
+    .then((people) => {
+      res.send(
+        `<p>Phonebook has info for ${people.length} people</p>`
+                + `<p>${new Date().toString()}</p>`,
+      );
+    });
+});
 
 app.get('/api/persons', (req, res) => {
-    Person
-        .find({})
-        .then(people => res.json(people))
-        .catch(error => next(error));
-})
+  Person
+    .find({})
+    .then((people) => res.json(people));
+});
 
-app.get('/api/persons/:id', (req, res) => {
-    Person
-        .findById(req.params.id)
-        .then(people => res.json(people))
-        .catch(error => next(error));
-})
+app.get('/api/persons/:id', (req, res, next) => {
+  Person
+    .findById(req.params.id)
+    .then((people) => res.json(people))
+    .catch((error) => next(error));
+});
 
-app.post('/api/persons', (req, res, next) => {    
-    const body = req.body;
-    
-    if (!body.name) {
-        return res.status(400).json({
-            error: 'Name is missing'
-        });
-    }
-    if (!body.number) {
-        return res.status(400).json({
-            error: 'Number is missing'
-        });
-    }
+app.post('/api/persons', (req, res, next) => {
+  const { body } = req;
 
-    const person = new Person ({
-        name: body.name,
-        number: body.number
-    })
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'Name is missing',
+    });
+  }
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'Number is missing',
+    });
+  }
 
-    person
-        .save()
-        .then(savedPerson => res.json(savedPerson))
-        .catch(error => next(error));
-})
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-app.delete('/api/persons/:id', (req, res) => {
-    Person 
-        .findByIdAndRemove(req.params.id)
-        .then(result => res.status(204).end())
-        .catch(error => next(error));
-})
+  person
+    .save()
+    .then((savedPerson) => res.json(savedPerson))
+    .catch((error) => next(error));
+});
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).end())
+    .catch((error) => next(error));
+});
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body;
+  const { body } = req;
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-    Person
-        .findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => res.json(updatedPerson))
-        .catch(error => next(error));
-})
+  Person
+    .findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => next(error));
+});
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message);
+  console.error(error.message);
 
-    if (error.name == 'CastError' && error.kind == 'ObjectId') {
-        return res.status(400).send({ error: 'malformatted id' });
-    }
-    else if (error.name === 'ValidationError') {
-        return res.status(400).send({ error: error.message})
-    }
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message });
+  }
 
-    next(error);
-}
+  next(error);
+};
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 app.listen(PORT, () => {
-    console.log(`Server running on portal ${PORT}`);
-})
+  console.log(`Server running on portal ${PORT}`);
+});
--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,61 +1,59 @@
 const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-    console.log('Please prvide the password as an argument: \n\tnode mongo.js <password>');
-    process.exit(1);
+  console.log('Please provide the password as an argument: \n\tnode mongo.js <password>');
+  process.exit(1);
 }
 
 if (process.argv.length > 3 && process.argv.length < 5) {
-    console.log('Please provide new entry as arguments: \n\tnode mongo.js <password> <name> <number>');
-    process.exit(1);
+  console.log('Please provide new entry as arguments: \n\tnode mongo.js <password> <name> <number>');
+  process.exit(1);
 }
 
 const password = process.argv[2];
 const url = `mongodb+srv://fullStackOpenUser:${password}@cluster0.e8ail.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
 mongoose
-    .connect(url, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-        useFindAndModify: false, 
-        useCreateIndex: true 
-    })
-    .catch(error => {
-        console.error(error);
-    });
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-// Using "mongoose.Schema.Types.Mixed" to take "numeric inputs with dashes"
-// Technically, that Schema type allows any type of input (seen as String on MongoDB)
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: mongoose.Schema.Types.Mixed
-})
+  name: String,
+  number: String,
+});
 
 const Person = mongoose.model('Person', personSchema);
 
 if (process.argv.length === 5) {
-    const person = new Person ({
-        name: process.argv[3],
-        number: process.argv[4]
-    });
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
 
-    person
-        .save()
-        .then(result => {
-            console.log(`added ${person.name} ${person.number} to phonebook`);
-            mongoose.connection.close();
-        })
-        .catch(error => {
-            console.error(error);
-        });
+  person
+    .save()
+    .then(() => {
+      console.log(`added ${person.name} ${person.number} to phonebook`);
+      mongoose.connection.close();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log("phonebook:");
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`);
-        });
-        mongoose.connection.close();
-    })
+  Person.find({}).then((result) => {
+    console.log('phonebook:');
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
 }
