30eeadd84b62ec16507f85d2ac82af7b37c93b2c
wrthlor <nickster179@gmail.com>
2021-01-21 00:16:01 -0600

    Exercises 4.1-4.2 - Turn starter code into functional npm app and refactored code into seperate modules

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const app = require('./app');
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/loggers');
+
+const server = http.createServer(app);
+
+const PORT = config.PORT;
+server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`);
+});
