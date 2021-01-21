9c5f3a2e0e5f36bb9bbb86c192854baaa2f2a56d
wrthlor <nickster179@gmail.com>
2021-01-21 16:48:53 -0600

    Exercise 4.4 - Helper functions and unit tests - Step 2 - Calculates total sum of likes from blogs

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,5 +1,19 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+    
+    if (blogs.length !== 0) {    
+        let sum = 0;
+        blogs.map(blog => sum += blog.likes);
+        
+        return sum;
+    }
+    else {
+        return 0;
+    }
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
