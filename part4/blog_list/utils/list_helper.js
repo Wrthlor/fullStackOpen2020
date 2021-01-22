ae62bc39c5d4ac56875e72aec70bd429fb25418a
wrthlor <nickster179@gmail.com>
2021-01-21 18:06:04 -0600

    Exercise 4.5 - Helper functions and unit tests - Step 3 - Returns single blog with highest likes

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,6 +1,10 @@
 const dummy = () => 1;
 
 const totalLikes = (blogs) => {
+    if (!blogs) {
+        return null;
+    }
+
     const total = blogs.reduce((accumulator, currentValue) => {
         return accumulator + currentValue.likes;
     }, 0);
@@ -8,7 +12,28 @@ const totalLikes = (blogs) => {
     return total;   
 }
 
+const favoriteBlog = (blogs) => {
+    if (!blogs || blogs.length === 0 ) {
+        return null;
+    }
+
+    const maxLikes = blogs.reduce((accumulator, currentValue) => {
+        return currentValue.likes > accumulator.likes
+            ? currentValue
+            : accumulator
+    })    
+
+    const favBlog = {
+        title: maxLikes.title,
+        author: maxLikes.author,
+        likes: maxLikes.likes
+    }
+    
+    return favBlog;
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
