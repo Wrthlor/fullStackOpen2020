fa2a88457f2e912983de7ff4cc31197ee4b02182
wrthlor <nickster179@gmail.com>
2021-02-08 18:09:11 -0600

    Exercises 4.15-4.16 - Implement HTTP-post request for username, name, password / includes unique username and password requirement, minlength restrictions

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -4,6 +4,8 @@ const helper = require('./test_helper');
 const app = require('../app');
 const api = supertest(app);
 
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 const Blog = require('../models/blog');
 
 beforeEach(async () => {
@@ -132,6 +134,95 @@ describe('deleting blog', () => {
     
         const titles = blogsAtEnd.map(blog => blog.title);
         expect(titles).not.toContain(blogToDelete.title);
+    })
+})
+
+describe('testing users', () => {
+    beforeEach(async () => {
+        await User.deleteMany({});
+
+        const passwordHash = await bcrypt.hash('sekret', 10);
+        const user = new User({ username: 'root', passwordHash });
+
+        await user.save();
+    })
+
+    test('user is returned', async () => {
+        const usersAtStart = await helper.usersInDb();    
+        expect(usersAtStart[0].username).toBe('root');
+    })
+
+    test('new username succeed', async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        const newUser = {
+            username: 'mluukkai', 
+            name: 'Matti Luukkainen', 
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/);
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+        const usernames = usersAtEnd.map(u => u.username);
+        expect(usernames).toContain(newUser.username);
+    })
+
+    test('existing username fails', async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        const newUser = {
+            username: 'root', 
+            name: 'Superuser',
+            password: 'password',
+        }
+        
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/);
+
+        expect(result.body.error).toContain('`username` to be unique');
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toHaveLength(usersAtStart.length);
+    })
+
+    test('invalid passwords', async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        let newUser = {
+            username: 'testUser',
+            name: 'testName'
+        }
+
+        let result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/);
+
+        expect(result.body.error).toContain('password is required');
+
+        newUser.password = '12';
+
+        result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/);
+
+        expect(result.body.error).toContain('password should be at least 3 characters');
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toHaveLength(usersAtStart.length);
     })
 })
 
